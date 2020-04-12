1513aa7d228005243c6947b0241487c961ff2352
Igor Belem <igortbelem@gmail.com>
2020-04-11 23:37:10 -0400

    directory restructuring

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
+//const bcrypt = require("bcryptjs");
 const server = express.Router();
 
-server.route("/").get((req, res, next) => {});
+server.route("/").get((req, res, next) => { });
 
 module.exports = server;
